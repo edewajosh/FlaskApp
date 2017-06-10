5e35529961690ba97aec6097efdabf207dd83ec5
edewajosh <joshedewa@gmail.com>
2017-06-10 11:03:31 +0300

    My First Commit

---
+++ data.py
@@ -0,0 +1,29 @@
+def Articles():
+	articles = [
+		{
+			'id' : 1,
+			'title' : 'Article One',
+			'body' : 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmodtempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodoconsequat. Duis aute irure dolor in reprehenderit in voluptate velit essecillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat nonproident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+			'author' :  'Josh Edewa',
+			'create_date' : '04-25-2017'
+
+		},
+		{
+			'id' : 2,
+			'title' : 'Article Two',
+			'body' : 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmodtempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodoconsequat. Duis aute irure dolor in reprehenderit in voluptate velit essecillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat nonproident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+			'author' :  'John Doe',
+			'create_date' : '04-25-2017'
+
+		},
+		{
+			'id' : 3,
+			'title' : 'Article Three',
+			'body' : 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmodtempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodoconsequat. Duis aute irure dolor in reprehenderit in voluptate velit essecillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat nonproident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+			'author' :  'Brad Traversy',
+			'create_date' : '04-25-2017'
+
+		}
+
+	]
+	return articles
